c8612eecb4bdcd29edf93edf39ccb3f2844952e1
Linda <129479896+Sorrelsprout@users.noreply.github.com>
2024-07-08 00:07:22 -0700

    Updated page with preliminary files

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    //JQuery code goes here
+    // $("#currentYear").html(new Date().getFullYear());
+
+    // $(".triplebar").click(function() {
+    //     $(".mobileMenu").css({ 
+    //         "transition": "all 0.5s cubic-bezier(0.5, 0, 0.5, 1)",
+    //         "transition-timing-function":"cubic-bezier(0.5, 0, 0.5, 1)"
+    //     });
+    //     $(".mobileMenu").toggleClass("expanded");
+    // });
+    // $(".mobile .mobileMenu a").click(function() {
+    //     $(".mobileMenu").removeClass("expanded");
+    // });
+
+    // $(".imgRotation").click(function(){ $(this).toggleClass("engaged"); }) // Zoom image when clicked
+
+    setTimeout(function() { $('#landing').fadeOut('fast'); }, 2000); // splash page
+
+    $(".featuredImage").click(function(){  // Zoom image when clicked
+        $(this).toggleClass("engaged");
+        $(this).parent().toggleClass("fullScreenImg");
+    });
+
+    $("#about").click(function(){ alert("🛠️ In progress! 🛠️"); });
+    $("#contact").click(function(){ alert("🛠️ In progress! 🛠️"); });
+})
