7507348ba3c722ac4b96d64299c66da1c8afd4ca
Linda <129479896+Sorrelsprout@users.noreply.github.com>
2024-07-08 22:21:27 -0700

    Mobile menu

--- scripts/script.js
+++ scripts/script.js
@@ -2,28 +2,29 @@ $(document).ready(function(){
     //JQuery code goes here
     // $("#currentYear").html(new Date().getFullYear());
 
-    // $(".triplebar").click(function() {
-    //     $(".mobileMenu").css({ 
-    //         "transition": "all 0.5s cubic-bezier(0.5, 0, 0.5, 1)",
-    //         "transition-timing-function":"cubic-bezier(0.5, 0, 0.5, 1)"
-    //     });
-    //     $(".mobileMenu").toggleClass("expanded");
-    // });
-    // $(".mobile .mobileMenu a").click(function() {
-    //     $(".mobileMenu").removeClass("expanded");
-    // });
+    // NAV -----------------------------------------------------------------------------------
 
-    // $(".imgRotation").click(function(){ $(this).toggleClass("engaged"); }) // Zoom image when clicked
+    $(".triplebar").click(function() {
+        $(".mobileMenu").css({ 
+            "transition": "all 0.5s cubic-bezier(0.5, 0, 0.5, 1)",
+            "transition-timing-function":"cubic-bezier(0.5, 0, 0.5, 1)"
+        });
+        $(".mobileMenu").toggleClass("expanded");
+    });
+
+    // SPLASH PAGE ---------------------------------------------------------------------------
 
     setTimeout(function() { $('#landing').fadeOut('fast'); }, 1500); // splash page
+
+    // ZOOM IMG ------------------------------------------------------------------------------
 
     $(".featuredImage").click(function(){  // Zoom image when clicked
         $(this).toggleClass("engaged");
         $(this).parent().toggleClass("fullScreenImg");
     });
 
-
     // NAV LINKS -----------------------------------------------------------------------------
+
     $("#about").click(function(){ 
         removeNavLinksAll();
         $("#navlinksPopup").addClass("engaged");
