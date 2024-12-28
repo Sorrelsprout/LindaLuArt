821551de34a7a64a24f8232831a210a5413a64f2
Linda <129479896+Sorrelsprout@users.noreply.github.com>
2024-12-27 20:12:08 -0800

    compiled styling

--- scripts/script.js
+++ scripts/script.js
@@ -47,4 +47,75 @@ $(document).ready(function(){
         $("#navlinksPopup").removeClass("contact");
     }
 
+    // GALLERY NAVIGATION ----------------------------------------------------------------------
+
+    // Using image selectors
+    let imageSelectors = document.querySelectorAll(".imageSelector");
+    imageSelectors.forEach(function (el) {
+        el.addEventListener("click", function(){
+            el.parentElement.querySelectorAll(".imageSelector").forEach(function (image) {
+                image.classList.remove("current");
+            })
+            el.classList.add("current");
+            let featuredImage = el.parentElement.parentElement.querySelector(".featuredImage");
+            // let AAA = "url('./images/" + el.getAttribute('data-fileName') + "')";
+
+            let src = "./images/" + el.getAttribute('data-fileName');
+            let str = 'url("' + src + '")';
+            featuredImage.style.backgroundImage = str;
+            console.log(str);
+        });
+    });
+
+    // Using arrows
+    let galleryToggles = document.querySelectorAll(".imageController");
+    let galleryTogglePrev = document.querySelectorAll(".imageController .prev");
+    let galleryToggleNext = document.querySelectorAll(".imageController .next");
+
+    galleryTogglePrev.forEach(function (el) {
+        el.addEventListener("click", function(){ navigateGallery(el, -1); });
+    });
+    galleryToggleNext.forEach(function (el) {
+        el.addEventListener("click", function(){ navigateGallery(el, 1); });
+    });
+
+    function navigateGallery(e, direction) {
+        let currentImageSelector = e.parentElement;
+        let currentGallery = currentImageSelector.parentElement;
+        let currentImagePosition = currentGallery.querySelector(".imageSelector.current");
+        let allImages = currentGallery.querySelectorAll(".imageSelector");
+
+        if(direction === 1) { // move forwards
+            let targetNext = currentImagePosition.nextElementSibling;
+            let firstImage = allImages[0];
+            if(typeof targetNext !== "undefined" && typeof targetNext !== null) {
+                if (!targetNext.classList.contains("imageSelector")) {
+                    // Next image unavailable, loop back to start
+                    navGal(firstImage);
+                } else { navGal(targetNext); }
+            }
+        }
+
+        if(direction === -1) { // move backwards
+            let targetPrev = currentImagePosition.previousElementSibling;
+            let lastImage = allImages[allImages.length - 1];
+            if(typeof targetPrev !== "undefined" && typeof targetPrev !== null) {
+                if (!targetPrev.classList.contains("imageSelector")) {
+                    // previous image unavailable, loop to last image
+                    navGal(lastImage);
+                } else { navGal(targetPrev); }
+            }
+        }
+
+        function navGal(targetType) {
+            currentGallery.querySelectorAll(".imageSelector").forEach(function (image) {
+                image.classList.remove("current");
+            })
+            targetType.classList.add("current");
+            let featuredImage = currentGallery.querySelector(".featuredImage");
+            let src = "./images/" + targetType.getAttribute('data-fileName');
+            let str = 'url("' + src + '")';
+            featuredImage.style.backgroundImage = str;
+        }
+    }
 })
